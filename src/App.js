e8c90111828623114c7879e8aa139399c9e06252
Kalpa D. Fernando <50633428+KalpaD98@users.noreply.github.com>
2023-03-13 12:46:48 +0530

    inital commit with all files

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import ChatWidget from './components/Widget'
+
+function App() {
+    return (<div className="App">
+            <ChatWidget/>
+        </div>);
+}
+
+export default App;
---
+++ src/components/Widget.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Widget from 'rasa-webchat';
+
+function ChatWidget() {
+
+    return (
+        <Widget
+            socketUrl={"http://localhost:5005"}
+            initPayload={"/greet"}
+            customData={{language: "en"}} // arbitrary custom data. Stay minimal as this will be added to the socket
+            title={"ResBot"}
+            subtitle={"Powered by Code Masters"}
+            //params={{"storage": "session"}}
+            autoClearCache={true}
+            customComponent={({text}) => <div><p>{text}{text}{text}</p></div>}
+            displayUnreadCount={true}
+            onWidgetEvent={{
+                onChatClose: () => {
+                    localStorage.clear();
+                },
+                onChatOpen: () => {
+                    localStorage.clear();
+                }
+            }}
+            params={{
+                storage: 'local',
+            }}
+        />
+    )
+}
+
+export default ChatWidget;
