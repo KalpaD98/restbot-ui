75a2071cb5f49cf9893e67db3f7384f5c9f8e450
Kalpa D. Fernando <50633428+KalpaD98@users.noreply.github.com>
2023-04-10 17:19:22 +0530

    minor changes added

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -4,26 +4,27 @@ import Widget from 'rasa-webchat';
 function ChatWidget() {
 
     return (<Widget
-            socketUrl={"http://192.168.8.102:5005"}
-            initPayload={"/greet"}
-            customData={{language: "en"}} // arbitrary custom data. Stay minimal as this will be added to the socket
-            title={"ResBot"}
-            subtitle={"Powered by Code Masters"}
-            //params={{"storage": "session"}}
-            autoClearCache={true}
-            customComponent={({text}) => <div><p>{text}{text}{text}</p></div>}
-            displayUnreadCount={true}
-            onWidgetEvent={{
-                onChatClose: () => {
-                    localStorage.clear();
-                }, onChatOpen: () => {
-                    localStorage.clear();
-                }
-            }}
-            params={{
-                storage: 'local',
-            }}
-        />)
+        // socketUrl={"http://192.168.8.102:5005"}
+        socketUrl={"http://localhost:5005"}
+        initPayload={"/greet"}
+        customData={{language: "en"}} // arbitrary custom data. Stay minimal as this will be added to the socket
+        title={"ResBot"}
+        subtitle={"Powered by Code Masters"}
+        //params={{"storage": "session"}}
+        autoClearCache={true}
+        customComponent={({text}) => <div><p>{text}{text}{text}</p></div>}
+        displayUnreadCount={true}
+        onWidgetEvent={{
+            onChatClose: () => {
+                localStorage.clear();
+            }, onChatOpen: () => {
+                localStorage.clear();
+            }
+        }}
+        params={{
+            storage: 'local',
+        }}
+    />)
 }
 
 export default ChatWidget;
