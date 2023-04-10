a26087edfcc0f0e1334c58dfed499c530716736e
Kalpa D. Fernando <50633428+KalpaD98@users.noreply.github.com>
2023-04-10 17:28:45 +0530

    before changing all 10 april 5 30 p m

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Widget from 'rasa-webchat';
 
 function ChatWidget() {
+    const myCustomData = {
+        inputTextFieldDelay: 300,
+        language: "en"
+    }
 
     return (<Widget
         // socketUrl={"http://192.168.8.102:5005"}
         socketUrl={"http://localhost:5005"}
         initPayload={"/greet"}
-        customData={{language: "en"}} // arbitrary custom data. Stay minimal as this will be added to the socket
+        customData={myCustomData} // arbitrary custom data. Stay minimal as this will be added to the socket
         title={"ResBot"}
         subtitle={"Powered by Code Masters"}
         //params={{"storage": "session"}}
